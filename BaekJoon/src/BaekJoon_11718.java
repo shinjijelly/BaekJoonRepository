09fb36b9b63233390659e033d82dc98912c4b57c
shinjijelly <shinjipark22@naver.com>
2024-07-17 23:05:22 +0900

    백준 11718번 (그대로 출력하기)

---
+++ BaekJoon/src/BaekJoon_11718.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BaekJoon_11718 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+       while(scanner.hasNext()){
+           String input = scanner.nextLine();
+           System.out.println(input);
+       }
+
+    }
+}
