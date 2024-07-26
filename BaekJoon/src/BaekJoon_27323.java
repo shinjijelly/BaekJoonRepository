d65527ae8642fb2bc9f1a53571009e74a1ad6885
shinjijelly <shinjipark22@naver.com>
2024-07-26 15:02:33 +0900

    백준 27323번 (직사각형)

---
+++ BaekJoon/src/BaekJoon_27323.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BaekJoon_27323 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A * B);
+    }
+}
