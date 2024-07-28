f38ed8bcc4216a83e30d77b6dd1f09acd943873b
shinjijelly <shinjipark22@naver.com>
2024-07-28 22:46:09 +0900

    백준 15894번 (수학은 체육과목 입니다)

---
+++ BaekJoon/src/BaekJoon_15894.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BaekJoon_15894 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+
+        System.out.println(n * 4);
+    }
+}
