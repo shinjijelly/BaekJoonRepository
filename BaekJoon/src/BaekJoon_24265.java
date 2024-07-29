b57fb8bba34b00a64a720d6e3847baf5d17cd142
shinjijelly <shinjipark22@naver.com>
2024-07-29 12:21:17 +0900

    백준 24265번 (알고리즘 수업 - 알고리즘의 수행 시간 4)

---
+++ BaekJoon/src/BaekJoon_24265.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BaekJoon_24265 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        long n = scanner.nextInt();
+
+        System.out.println(n * (n - 1) / 2);
+        System.out.println("2");
+    }
+
+}
